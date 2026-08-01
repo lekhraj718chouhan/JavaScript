e5f3d6b083734f7fbb664d523fc4fe9f4a2355d9
lekhraj718chouhan <160110292+lekhraj718chouhan@users.noreply.github.com>
2026-08-01 10:00:01 +0530

    Add JavaScript files for prototype methods and color changer functionality

---
+++ JS/Prototype.js
@@ -0,0 +1,57 @@
+// let myName = "lekhraj    "
+
+// console.log(myName.truelength);
+
+
+
+let myHeros = ["thor", "spiderman"]
+
+
+let heroPower = {
+    thor: "hammer",
+    spiderman: "sling",
+
+    getSpiderPower: function(){
+        console.log(`Spidy power is ${this.spiderman}`)
+    }
+}
+
+Object.prototype.lekhraj = function(){
+    console.log(`lekhraj is present in all objects`);
+}
+Array.prototype.heylekhraj = function(){
+    console.log(`Lekhraj says hello`);
+}
+
+// heroPower.lekhraj()
+myHeros.lekhraj()
+myHeros.heylekhraj()
+// heroPower.heylekhraj()
+
+
+// inheritance
+
+const user = {
+    name: "chai",
+    email: "chai@googlegmail.com"
+}
+
+const Teacher = {
+    makeVideo: true
+}
+
+const TeachingSupport = {
+    isAvailable: false
+}
+
+const TASupport = {
+    makeAssignment: 'JS assigment',
+    fullTime: true,
+    __proto__: TeachingSupport
+}
+
+Teacher.__proto__ = User
+
+// modern syntax
+
+Object.setPrototypeOf(TeachingSupport, Teacher)
