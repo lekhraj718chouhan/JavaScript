659230bee46aa8378780f7a3c7a6f3e8374a906e
lekhraj718chouhan <160110292+lekhraj718chouhan@users.noreply.github.com>
2026-08-02 11:21:45 +0530

    Add examples of type conversion and boolean evaluation in Datatypes.js

--- JS/Datatypes.js
+++ JS/Datatypes.js
@@ -22,3 +22,32 @@ console.log(typeof "lekhraj")
 console.log(typeof null)
 console.log(typeof undefined)
 
+
+
+
+let score = "33a"
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+
+// "33" => 33
+//"33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedInn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedInn);
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "lekharj" => true 
+
+
+let someNumber = 45
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber)
