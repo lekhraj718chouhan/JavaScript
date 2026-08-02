14c1d8e3fee69188c0e7a681c45a9cce7d03e569
lekhraj718chouhan <160110292+lekhraj718chouhan@users.noreply.github.com>
2026-08-02 10:43:02 +0530

    Add Datatypes.js file with variable declarations and type logging examples

---
+++ JS/Datatypes.js
@@ -0,0 +1,24 @@
+"use strict";  // treat all JS code as newer version 
+
+// alert(3+3)   we are using nodejs, not brower
+
+let name = "lekhraj"
+let age = 21
+let isLoggedIn = true
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique 
+
+
+// object
+
+console.log(typeof "lekhraj")
+console.log(typeof null)
+console.log(typeof undefined)
+
